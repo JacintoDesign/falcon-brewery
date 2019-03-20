4f6e706f267bec11d41f5cc59f95b73a2c91e99d
Jacinto Wong <contact@jacinto.design>
2019-03-20 15:10:40 -0400

    finalized beer page for monitor/laptop

--- src/app/components/beer/beer.component.ts
+++ src/app/components/beer/beer.component.ts
@@ -12,4 +12,166 @@ export class BeerComponent implements OnInit {
   ngOnInit() {
   }
 
+  hideCard1() {
+    let card = document.getElementById("card-1");
+    card.classList.add("no-show")
+  }
+
+  hideCard2() {
+    let card = document.getElementById("card-2");
+    card.classList.add("no-show")
+  }
+
+  hideCard3() {
+    let card = document.getElementById("card-3");
+    card.classList.add("no-show")
+  }
+
+  hideCard4() {
+    let card = document.getElementById("card-4");
+    card.classList.add("no-show")
+  }
+
+  hideCard5() {
+    let card = document.getElementById("card-5");
+    card.classList.add("no-show")
+  }
+
+  hideCard6() {
+    let card = document.getElementById("card-6");
+    card.classList.add("no-show")
+  }
+
+  hideCard7() {
+    let card = document.getElementById("card-7");
+    card.classList.add("no-show")
+  }
+
+  hideCard8() {
+    let card = document.getElementById("card-8");
+    card.classList.add("no-show")
+  }
+
+  hideCard9() {
+    let card = document.getElementById("card-9");
+    card.classList.add("no-show")
+  }
+
+  showCard1() {
+    let card = document.getElementById("card-1");
+    card.classList.remove("no-show");
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard4();
+    this.hideCard5();
+    this.hideCard6();
+    this.hideCard7();
+    this.hideCard8();
+    this.hideCard9();
+  }
+
+  showCard2() {
+    let card = document.getElementById("card-2");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard3();
+    this.hideCard4();
+    this.hideCard5();
+    this.hideCard6();
+    this.hideCard7();
+    this.hideCard8();
+    this.hideCard9();
+  }
+
+  showCard3() {
+    let card = document.getElementById("card-3");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard4();
+    this.hideCard5();
+    this.hideCard6();
+    this.hideCard7();
+    this.hideCard8();
+    this.hideCard9();
+  }
+
+  showCard4() {
+    let card = document.getElementById("card-4");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard5();
+    this.hideCard6();
+    this.hideCard7();
+    this.hideCard8();
+    this.hideCard9();
+  }
+
+  showCard5() {
+    let card = document.getElementById("card-5");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard4();
+    this.hideCard6();
+    this.hideCard7();
+    this.hideCard8();
+    this.hideCard9();
+  }
+
+  showCard6() {
+    let card = document.getElementById("card-6");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard5();
+    this.hideCard4();
+    this.hideCard7();
+    this.hideCard8();
+    this.hideCard9();
+  }
+
+  showCard7() {
+    let card = document.getElementById("card-7");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard5();
+    this.hideCard6();
+    this.hideCard4();
+    this.hideCard8();
+    this.hideCard9();
+  }
+
+  showCard8() {
+    let card = document.getElementById("card-8");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard5();
+    this.hideCard6();
+    this.hideCard7();
+    this.hideCard4();
+    this.hideCard9();
+  }
+
+  showCard9() {
+    let card = document.getElementById("card-9");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard5();
+    this.hideCard6();
+    this.hideCard7();
+    this.hideCard4();
+    this.hideCard8();
+  }
+
 }
