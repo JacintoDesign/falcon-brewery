63ed791899df00c8d6f64e9166a0c524a6258e16
Jacinto Wong <contact@jacinto.design>
2019-03-21 22:48:10 -0400

    started on tap page

--- src/app/components/social/social.component.ts
+++ src/app/components/social/social.component.ts
@@ -12,4 +12,56 @@ export class SocialComponent implements OnInit {
   ngOnInit() {
   }
 
+  hideCard1() {
+    let card = document.getElementById("card-1");
+    card.classList.add("no-show")
+  }
+
+  hideCard2() {
+    let card = document.getElementById("card-2");
+    card.classList.add("no-show")
+  }
+
+  hideCard3() {
+    let card = document.getElementById("card-3");
+    card.classList.add("no-show")
+  }
+
+  hideCard4() {
+    let card = document.getElementById("card-4");
+    card.classList.add("no-show")
+  }
+
+  showCard1() {
+    let card = document.getElementById("card-1");
+    card.classList.remove("no-show");
+    this.hideCard2();
+    this.hideCard3();
+    this.hideCard4();
+  }
+
+  showCard2() {
+    let card = document.getElementById("card-2");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard3();
+    this.hideCard4();
+  }
+
+  showCard3() {
+    let card = document.getElementById("card-3");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard4();
+  }
+
+  showCard4() {
+    let card = document.getElementById("card-4");
+    card.classList.remove("no-show");
+    this.hideCard1();
+    this.hideCard2();
+    this.hideCard3();
+  }
+
 }
